8133bee656b983e29fa128a2eb2301196b6c333d
isavlex <mailsavlex@gmail.com>
2020-03-03 20:26:27 +0300

    Connected, configured and redesigned paginationjs

--- src/static/js/formElements.js
+++ src/static/js/formElements.js
@@ -1,21 +1,42 @@
 require("../../components/calendar/datepicker.js");
 import { howMany } from "../../components/dropdown/dropdown";
-
+import pagination from "paginationjs/dist/pagination";
 import noUiSlider from "nouislider/distribute/nouislider";
-// import { showHide } from "../../components/switches/switches";
 
-
-// toggleClass(".dropdown__heading", "dropdown__wrap--active");
-
-// showHide(".checkbox-list__heading", ".checkbox-list__list");
-// showHide({
-//     clickEl: ".switches__heading",
-//     clickToggleClass: "switches__heading--shown",
-//     showHideEl: ".switches__list",
-//     showHideToggleClass: "switches__list--shown" 
-// });
 howMany();
 
+//start-pagination
+
+
+
+//end-pagination
+
+function simpleTemplating(data) {
+    var html = '<ul>';
+    $.each(data, function(index, item){
+        html += '<li>'+ item +'</li>';
+    });
+    html += '</ul>';
+    return html;
+}
+
+$('#example-pagination').pagination({
+    dataSource: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    locator: 'items',
+    totalNumber: 15,
+    pageSize: 1,
+    pageRange: 1,
+    showPrevious: false,
+    showNext: true,
+    nextText: '<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 0.984375L17.0156 9L9 17.0156L7.59375 15.6094L13.1719 9.98438H0.984375V8.01562H13.1719L7.59375 2.39062L9 0.984375Z" fill="white"/></svg>',
+    // callback: function(data, pagination) {
+    //     // template method of yourself
+    //     var html = simpleTemplating(data);
+    //     $('#data-container').html(html);
+    // }
+})
+
+//start-nouislider
 let myRangeSlider = document.getElementById("fe-rslider");
 
 let snapValues = [
@@ -38,6 +59,7 @@ noUiSlider.create(myRangeSlider, {
 myRangeSlider.noUiSlider.on('update', function (values, handle) {
     snapValues[handle].innerHTML = Math.round(values[handle]) + "₽";
 });
+//end-nouislider
 
 $(document).ready(function () {
     $('.dropdown__heading--from-reservation, .dropdown__heading--to-reservation').datepicker({
