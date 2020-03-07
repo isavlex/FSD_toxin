0db3211000f99aa1976cfb676bb2c55642f0483f
isavlex <mailsavlex@gmail.com>
2020-03-07 18:05:28 +0300

    Calendar call changed in cards

--- src/static/js/cards.js
+++ src/static/js/cards.js
@@ -1,7 +1,7 @@
 require("../../components/calendar/datepicker.js");
 
 $(document).ready(function () {
-    $('.selection__choice--arrival, .selection__choice--departure').datepicker({
+    $('.selection__choice--arrival').datepicker({
         altField: '.selection__choice--departure',
         multipleDatesSeparator: ' ',
         range: true,
@@ -27,4 +27,31 @@ $(document).ready(function () {
             secondInput.val(second);
         }
     });
+    $('.selection__choice--departure').datepicker({
+        altField: '.selection__choice--arrival',
+        multipleDatesSeparator: ' ',
+        range: true,
+        clearButton: true,
+        confirmButton: true,
+        position: "bottom right",
+        onSelect: function (fd, dates, inst) {
+            var firstInput = $(this.altField);
+            var secondInput = $('.selection__choice--departure');
+            var first, second;
+
+            dates.forEach(function (date, index) {
+
+                let rightDate = (date.getDate() < 10) ? ('0' + date.getDate()) : date.getDate();
+                let rightMonth = ((date.getMonth() + 1) < 10) ? ('0' + (date.getMonth() + 1)) : date.getMonth() + 1;
+                if (index == 0) {
+                    first = rightDate + "." + rightMonth + "." + date.getFullYear();
+                } else {
+                    second = rightDate + "." + rightMonth + "." + date.getFullYear();
+                }
+            })
+
+            firstInput.val(first);
+            secondInput.val(second);
+        }
+    })
 });
