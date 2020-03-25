21962a5e435a0a383bd11ddfcec8c3f7c5af43d7
isavlex <mailsavlex@gmail.com>
2020-03-25 19:46:11 +0300

    Changed 'card', added values in 'dropdown'

--- src/static/js/cards.js
+++ src/static/js/cards.js
@@ -6,7 +6,10 @@ setMask('example__dob');
 setMask('example__email', 'email');
 
 howMany("cards__example");
+howMany("cards__example22");
+
 $(document).ready(function () {
+    //reservation
     $('.dropdown__heading--from-reservation').datepicker({
         altField: '.dropdown__heading--to-reservation',
         multipleDatesSeparator: ' ',
@@ -42,6 +45,59 @@ $(document).ready(function () {
         onSelect: function (fd, dates, inst) {
             var firstInput = $(this.altField);
             var secondInput = $('.dropdown__heading--to-reservation');
+            var first, second;
+            console.log($(this))
+            dates.forEach(function (date, index) {
+
+                let rightDate = (date.getDate() < 10) ? ('0' + date.getDate()) : date.getDate();
+                let rightMonth = ((date.getMonth() + 1) < 10) ? ('0' + (date.getMonth() + 1)) : date.getMonth() + 1;
+                if (index == 0) {
+                    first = rightDate + "." + rightMonth + "." + date.getFullYear();
+                } else {
+                    second = rightDate + "." + rightMonth + "." + date.getFullYear();
+                }
+            })
+
+            firstInput.val(first);
+            secondInput.val(second);
+        }
+    })
+    //cards-example
+    $('.dropdown__heading--from-cards-example').datepicker({
+        altField: '.dropdown__heading--to-cards-example',
+        multipleDatesSeparator: ' ',
+        range: true,
+        clearButton: true,
+        confirmButton: true,
+        onSelect: function (fd, dates, inst) {
+            var firstInput = $('.dropdown__heading--from-cards-example');
+            var secondInput = $(this.altField);
+            var first, second;
+            dates.forEach(function (date, index) {
+
+                let rightDate = (date.getDate() < 10) ? ('0' + date.getDate()) : date.getDate();
+                let rightMonth = ((date.getMonth() + 1) < 10) ? ('0' + (date.getMonth() + 1)) : date.getMonth() + 1;
+                if (index == 0) {
+                    first = rightDate + "." + rightMonth + "." + date.getFullYear();
+                } else {
+                    second = rightDate + "." + rightMonth + "." + date.getFullYear();
+                }
+            })
+
+            firstInput.val(first);
+            secondInput.val(second);
+        }
+    });
+    $('.dropdown__heading--to-cards-example').datepicker({
+        altField: '.dropdown__heading--from-cards-example',
+        multipleDatesSeparator: ' ',
+        range: true,
+        clearButton: true,
+        confirmButton: true,
+        position: "bottom right",
+        onSelect: function (fd, dates, inst) {
+            var firstInput = $(this.altField);
+            var secondInput = $('.dropdown__heading--to-cards-example');
             var first, second;
             console.log($(this))
             dates.forEach(function (date, index) {
